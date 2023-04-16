4c40a5ada7ef09fcd1b618fadf0c10a0b2d2dd42
sksat <sksat@sksat.net>
2023-04-16 23:11:13 +0900

    add current build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget 'https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_Linux-64bit.tar.gz'
+tar xvf ./hugo_extended_0.111.3_Linux-64bit.tar.gz
+./hugo
