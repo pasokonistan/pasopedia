7c7f196e5a96b414766e36784c77545910e6c9d6
sksat <sksat@sksat.net>
2023-04-30 17:17:37 +0900

    extract hugo in temporary dir

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")" || exit
 
-wget 'https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_Linux-64bit.tar.gz'
-tar xvf ./hugo_extended_0.111.3_Linux-64bit.tar.gz
-./hugo
+mkdir tmp
+wget 'https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_Linux-64bit.tar.gz' -O tmp/hugo.tar.gz
+tar xvf ./tmp/hugo.tar.gz --directory=./tmp
+./tmp/hugo
